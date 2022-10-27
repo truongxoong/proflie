e1c0155f8fec8beeb2470dfe3b293b3a43078695
Truong Cu <truongxoongx3@gmail.com>
2022-10-27 17:28:12 +0700

    edit slider

--- src/component/experience/Item.jsx
+++ src/component/experience/Item.jsx
@@ -1,8 +1,10 @@
-import './about.css'
+import "./about.css";
 function Item(props) {
-    return <div className="flex text-lg font-medium pb-3 aboutItem">
-                <div className="text-teal-600">{props.keyy}</div>
-                <div className="text-slate-300 pl-1">{props.value}</div>
-            </div>
+  return (
+    <div className="flex text-lg font-medium pb-3 aboutItem">
+      <div className="text-teal-600">{props.keyy}</div>
+      <div className="text-slate-300 pl-1">{props.value}</div>
+    </div>
+  );
 }
-export default Item
+export default Item;
--- src/component/slider/index.js
+++ src/component/slider/index.js
@@ -1,48 +1,59 @@
-import { UserOutlined , ContactsOutlined , ExperimentOutlined , HomeOutlined } from '@ant-design/icons';
-import { NavLink } from 'react-router-dom';
-import './slider.css'
+import {
+  UserOutlined,
+  ContactsOutlined,
+  ExperimentOutlined,
+  HomeOutlined,
+} from "@ant-design/icons";
+import { NavLink } from "react-router-dom";
+import "./slider.css";
 const sliderMenu = [
-    {
-        icon: <HomeOutlined />,
-        title: 'Home',
-        to: '/home'
-    },
-    {
-        icon: <UserOutlined  />,
-        title: 'About Me',
-        to: '/about'
-    },
-    {
-        icon: <ExperimentOutlined  />,
-        title: 'Skill',
-        to: '/skill'
-    },
-    {
-        icon: <ExperimentOutlined  />,
-        title: 'Product',
-        to: '/product'
-    },
-    {
-        icon: <ContactsOutlined  />,
-        title: 'Contact',
-        to: '/contact'
-    },
-]
+  {
+    icon: <HomeOutlined />,
+    title: "Home",
+    to: "/home",
+  },
+  {
+    icon: <UserOutlined />,
+    title: "About Me",
+    to: "/about",
+  },
+  {
+    icon: <ExperimentOutlined />,
+    title: "Skill",
+    to: "/skill",
+  },
+  {
+    icon: <ExperimentOutlined />,
+    title: "Product",
+    to: "/product",
+  },
+  {
+    icon: <ContactsOutlined />,
+    title: "Contact",
+    to: "/contact",
+  },
+];
 let activeStyle = {
-    color:'white'
+  color: "red",
 };
 let noActiveStyle = {
-    color:'#1ab394'
+  color: "#1ab394",
 };
 function Slider() {
-    return <div className="text-center text-lg font-bold sliderCss">
-    {
-        sliderMenu.map((item) => <NavLink style={({ isActive }) =>isActive ? activeStyle : noActiveStyle} key={item.title} className='flex py-3 cursor-pointer text-white' to={item.to}>
-            <div className='w-[20%] icon'>{item.icon}</div>
-            <div className='pt-1'>{item.title}</div>
-        </NavLink>)
-    }
-        
+  return (
+    <div className="text-center text-lg font-bold sliderCss">
+      {sliderMenu.map((item) => (
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyle : noActiveStyle)}
+          key={item.title}
+          className="flex py-3 cursor-pointer text-white"
+          to={item.to}
+        >
+          <div className="w-[20%] icon">{item.icon}</div>
+          <div className="pt-1">{item.title}</div>
+        </NavLink>
+      ))}
     </div>
+  );
 }
-export default Slider
+export default Slider;
